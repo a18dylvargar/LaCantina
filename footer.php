3361a33bd26a1982dcbe03f8338256fc5a593545
a18dylvargar <a18dylvargar@inspedralbes.cat>
2021-10-29 11:51:16 +0200

    Footer mejorado

--- footer.php
+++ footer.php
@@ -6,26 +6,16 @@ echo "<div id='imp'>";
         echo "</div>";
         echo "<div id='dos'>";
             echo "<div  id='tres'><h1>Legal</h1></div>";
-            echo "<div id='one'>";
-                "Cookies
-                Avís legal
-                Protecció de dades";
-            echo "</div>";
+            echo "<div id='one'>Cookies,Avís legal,Protecció de dades</div>";
         echo "</div>";
         echo "<div id='dos'>";
             echo "<div  id='tres'><h1>Contacte</h1></div>";
-            echo "<div id='one'>";
-                "93 203 33 32
-                93 203 36 42
-                93 203 83 86
-                inspedralbes@xtec.cat
-                Web AFA";
-            echo "</div>";
+            echo "<div id='one'>93 203 33 32,93 203 36 42,93 203 83 86,inspedralbes@xtec.cat,Web AFA</div>";
         echo "</div>";
         echo "<div id='dos'>";
             echo "<div  id='tres'><h1>Adreça</h1></div>";
             echo "<div id='divimg'>";
-                echo "<img src='/img/mapa.PNG'>";
+                echo "<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5986.76851302333!2d2.109613467229756!3d41.387460787127004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a49854a99cad2f%3A0xb564263b0575d513!2sInstitut%20Pedralbes!5e0!3m2!1sca!2ses!4v1635405909805!5m2!1sca!2ses' width='250' height='200' style='border:0;' allowfullscreen='' loading='lazy'></iframe>";
             echo "</div>";
         echo "</div>";
     echo "</div>";
